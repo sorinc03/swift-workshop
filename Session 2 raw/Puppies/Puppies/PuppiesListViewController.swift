bd99ecef3220c0c2f4071a73eb89472dda3c9bd6
Cesare De Cal <hi@cesare.io>
2019-08-01 14:54:49 +0100

    Added session 2 raw project

---
+++ Session 2 raw/Puppies/Puppies/PuppiesListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  PuppiesListViewController.swift
+//  Puppies
+//
+//  Created by Cesare De Cal on 30/07/2019.
+//  Copyright © 2019 Iulia Monica Baltoi. All rights reserved.
+//
+
+import UIKit
+
+class PuppiesListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    let puppyId = "PuppyId"
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10000
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: puppyId, for: indexPath) as! PuppyCellTableViewCell
+        cell.textLabel?.text = "Puppy \(indexPath.row)"
+//        cell.imageView?.image = UIImage
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Session 2 raw/Puppies/Puppies/PuppyCellTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  PuppyCellTableViewCell.swift
+//  Puppies
+//
+//  Created by Cesare De Cal on 30/07/2019.
+//  Copyright © 2019 Iulia Monica Baltoi. All rights reserved.
+//
+
+import UIKit
+
+class PuppyCellTableViewCell: UITableViewCell {
+    let imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
