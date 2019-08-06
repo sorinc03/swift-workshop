d20555b84b328819462f41452dc1bdb0bc873b55
Iulia Monica Baltoi <ibaltoi@twitter.com>
2019-08-06 10:13:05 +0100

    Update with custom cell

--- Session 2 raw/Puppies/Puppies/PuppiesListViewController.swift
+++ Session 2 raw/Puppies/Puppies/PuppiesListViewController.swift
@@ -10,38 +10,48 @@ import UIKit
 
 class PuppiesListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     let puppyId = "PuppyId"
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10000
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: puppyId, for: indexPath)
-        cell.textLabel?.text = "Puppy \(indexPath.row)"
-        cell.imageView?.image = UIImage(named: "pic2.jpg")
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
+    var puppyArray = [Puppy]()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        for i in 0..<10 {
+            guard let image = UIImage(named: "Puppy-pics/puppy-\(i).jpg") else {
+                return
+            }
+            let puppy = Puppy(image: image, name: "Puppy \(i)")
+            puppyArray.append(puppy)
+        }
+    }
+
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 105
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return puppyArray.count
     }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: puppyId, for: indexPath) as! PuppyTableViewCell
+        cell.puppyTitleLabel?.text = "Puppy \(indexPath.row)"
+        cell.puppyImageView?.image =  UIImage(named: "Puppy-pics/puppy-\(indexPath.row).jpg")
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        return cell
     }
-    */
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+class Puppy {
+    let image: UIImage
+    let name: String
+
+    init(image: UIImage, name: String) {
+        self.image = image
+        self.name = name
+    }
 }
---
+++ Session 2 raw/Puppies/Puppies/PuppyTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  PuppyTableViewCell.swift
+//  Puppies
+//
+//  Created by Iulia Monica Baltoi on 8/5/19.
+//  Copyright © 2019 Iulia Monica Baltoi. All rights reserved.
+//
+
+import UIKit
+
+class PuppyTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var puppyImageView: UIImageView!
+    @IBOutlet weak var puppyTitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
