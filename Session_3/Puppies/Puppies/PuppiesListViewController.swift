3331412eebf02a99ffc682ad9978f026e4d7d9a2
Iulia Monica Baltoi <ibaltoi@twitter.com>
2019-08-13 15:01:39 +0100

    Update session3

--- Session_3/Puppies/Puppies/PuppiesListViewController.swift
+++ Session_3/Puppies/Puppies/PuppiesListViewController.swift
@@ -10,16 +10,18 @@ import UIKit
 
 class PuppiesListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     let puppyId = "PuppyId"
+
+    var puppies = [Puppy]()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10000
+        return puppies.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: puppyId, for: indexPath)
-        cell.textLabel?.text = "Puppy \(indexPath.row)"
-//        cell.imageView?.image = UIImage(named: "pic2.jpg")
-        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: puppyId, for: indexPath) as? PuppyTableViewCell
+        cell?.puppyNameLabel?.text = puppies[indexPath.row].name
+        cell?.puppyImageView?.image = puppies[indexPath.row].image
+        return cell!
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -30,6 +32,11 @@ class PuppiesListViewController: UIViewController, UITableViewDataSource, UITabl
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        for puppyIndex in 1...10 {
+            let image = UIImage(named: "puppy-\(puppyIndex).jpg")
+            let puppy = Puppy(name: "Puppy \(puppyIndex)", image: image)
+            puppies.append(puppy)
+        }
         // Do any additional setup after loading the view.
     }
     
@@ -44,4 +51,14 @@ class PuppiesListViewController: UIViewController, UITableViewDataSource, UITabl
     }
     */
 
+}
+
+class Puppy {
+    var name: String?
+    var image: UIImage?
+
+    init(name: String?, image: UIImage?) {
+        self.name = name
+        self.image = image
+    }
 }
